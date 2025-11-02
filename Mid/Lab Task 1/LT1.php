dee0e92929bc7b694975d67ff16f8a2039e40a89
Yaqzan <shamvi.abdullah@gmail.com>
2025-11-02 13:51:52 +0600

    Basic

--- Mid/Lab Task 1/LT1.php
+++ Mid/Lab Task 1/LT1.php
@@ -1,8 +1,27 @@
-<!DOCTYPE>
+<!DOCTYPE html>
 <html>
 <head>
-    <title></title>
+    <title> Clinic Patient Registration</title>
 </head>
 <body>
+<center>
+<h1> Clinic Patient Registration</h1>
+Full Name:<br>
+<input type="text"><br>
+Age:<br>
+<input type="text"><br>
+Phone Number:<br>
+<input type="text"><br>
+Email Address:<br>
+<input type="text"><br>
+Insurance Provider:<br>
+<select>
+    <option>Select Provider</option>
+    <option>Provider A</option>
+    <option>Provider B</option>
+    <option>Provider C</option>
+</select>
+
+</center>
 </body>
 </html>
