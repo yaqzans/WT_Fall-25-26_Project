d1859f95ea3bf89dd9332a0eb3a1c7bd219a8447
Yaqzan <shamvi.abdullah@gmail.com>
2025-11-02 14:22:08 +0600

    Almost

--- Mid/Lab Task 1/LT1.php
+++ Mid/Lab Task 1/LT1.php
@@ -5,35 +5,38 @@
 </head>
 <body>
 <center>
-<h1  style = color:Blue> Clinic Patient Registration</h1>
+<h1 style = color:DarkBlue> Clinic Patient Registration</h1>
 <div>
+<p class = "left">
 Full Name:<br>
-<input type="text"><br>
+<input type="text" class ="w"><br>
 Age:<br>
-<input type="text"><br>
+<input type="text" class ="w"><br>
 Phone Number:<br>
-<input type="text"><br>
+<input type="text" class ="w"><br>
 Email Address:<br>
-<input type="text"><br>
+<input type="text" class ="w"><br>
 Insurance Provider:<br>
 <select>
-    <option>Select Provider</option>
+    <option>              Select Provider              </option>
     <option>Provider A</option>
     <option>Provider B</option>
     <option>Provider C</option>
 </select>
 <br><br>
 Insurance Policy Number:<br>
-<input type="text"><br>
-<h3>Additional Information</h3>
+<input type="text" class ="w"><br>
+<h3 style = color:DarkBlue>Additional Information</h3>
+<p class = "left">
 Username:<br>
-<input type="text"><br>
+<input type="text" class ="w"><br>
 Password:<br>
-<input type="text"><br>
+<input type="text" class ="w"><br>
 Confirm Password:<br>
-<input type="text"><br><br>
-
+<input type="text" class ="w"><br><br>
+</p>
 <input type = "submit" value="Register">
+</p>
 </div>
 </center>
 </body>
@@ -43,6 +46,13 @@ div{
     width: 300px;
     padding: 10px;
     background-color: Cornsilk;
+}
+.left{
+    text-align: left;
+    align: left;
+}
+.w{
+    width: 280px;
 }
 </style>
 </html>
