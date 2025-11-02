3e8ea73c24b2124f565c5309a27b0acb50e14865
Yaqzan <shamvi.abdullah@gmail.com>
2025-11-02 14:08:13 +0600

    Basic_Frame

--- Mid/Lab Task 1/LT1.php
+++ Mid/Lab Task 1/LT1.php
@@ -5,7 +5,8 @@
 </head>
 <body>
 <center>
-<h1> Clinic Patient Registration</h1>
+<h1  style = color:Blue> Clinic Patient Registration</h1>
+<div>
 Full Name:<br>
 <input type="text"><br>
 Age:<br>
@@ -21,7 +22,27 @@ Insurance Provider:<br>
     <option>Provider B</option>
     <option>Provider C</option>
 </select>
+<br><br>
+Insurance Policy Number:<br>
+<input type="text"><br>
+<h3>Additional Information</h3>
+Username:<br>
+<input type="text"><br>
+Password:<br>
+<input type="text"><br>
+Confirm Password:<br>
+<input type="text"><br><br>
 
+<input type = "submit" value="Register">
+</div>
 </center>
 </body>
+<style>
+div{
+    border: 2px solid black;
+    width: 300px;
+    padding: 10px;
+    background-color: Cornsilk;
+}
+</style>
 </html>
