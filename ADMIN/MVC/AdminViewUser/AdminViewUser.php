4b998866f628af6fa4814c0b17a3747a57efb3e8
Yaqzan <shamvi.abdullah@gmail.com>
2026-01-19 19:26:12 +0600

    queries seperated

--- ADMIN/MVC/AdminViewUser/AdminViewUser.php
+++ ADMIN/MVC/AdminViewUser/AdminViewUser.php
@@ -1,5 +1,7 @@
 <?php
 include "../db/asc.php";
+include "../db/avu.php";
+
 if (!isset($_GET['id'])) 
 {
     die("User not found"); 
@@ -12,10 +14,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 { //I tried to use the empty error method but it wasnt working
     if ($_POST["action"] == "delete") 
     {
-        mysqli_query($conn, "DELETE FROM sessions WHERE user_id = $uid");
-        mysqli_query($conn, "DELETE FROM campaigns WHERE user_id = $uid");
-        mysqli_query($conn, "DELETE FROM profiles WHERE user_id = $uid");
-        mysqli_query($conn, "DELETE FROM users WHERE id = $uid");
+        avu_delete_user($uid);
         $userDeleted = true;
     }
     if (empty($_POST["amount"]) && $_POST["action"] != "delete") 
@@ -34,29 +33,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
         {
             if ($_POST["action"] == "add") 
             {
-                mysqli_query($conn,"UPDATE profiles SET credits = credits + $amount WHERE user_id = $uid");
-                mysqli_query($conn,"INSERT INTO ledger (sender_id, receiver_id, amount) VALUES (NULL, $uid, $amount)");
+                avu_add_credits($uid, $amount);
             }
 
             if ($_POST["action"] == "remove") 
             {
-                mysqli_query($conn,"UPDATE profiles SET credits = credits - $amount WHERE user_id = $uid");
-                mysqli_query($conn,"INSERT INTO ledger (sender_id, receiver_id, amount) VALUES ($uid, NULL, $amount)");
+                avu_remove_credits($uid, $amount);
             }
         }
     }
     $amount = "";
 }
-$user_res = mysqli_query($conn, "SELECT * FROM users WHERE id = $uid"); //This is for general info
+$user_res = avu_get_user($uid); //This is for general info
 $user = mysqli_fetch_assoc($user_res);
 if ($userDeleted || !$user) 
 {
     echo "<p style='font-weight:bold; text-align:center; font-size:32px;'>User no longer exists</p>";
     return;
 }
-$profile_res = mysqli_query($conn, "SELECT * FROM profiles WHERE user_id = $uid"); //This is for profile info
+$profile_res = avu_get_profile($uid); //This is for profile info
 $profile = mysqli_fetch_assoc($profile_res);
-$camp_res = mysqli_query($conn, "SELECT * FROM campaigns WHERE user_id = $uid"); //This is for survey history
+$camp_res = avu_get_campaigns($uid); //This is for survey history
 ?>
 <!DOCTYPE html>
 <html>
